72f80253ea9d9bc6aabc52956b50c0970176cb71
riko <rikokurniasandi07@gmail.com>
2026-07-16 18:57:32 +0700

    chore: clean up temporary patch files

--- patch.py
+++
@@ -1,5 +0,0 @@
-with open("app/components/LandingPage.tsx", "r") as f:
-    lines = f.readlines()
-lines.insert(-2, "    </div>\n")
-with open("app/components/LandingPage.tsx", "w") as f:
-    f.writelines(lines)
--- patch2.py
+++
@@ -1,4 +0,0 @@
-with open("app/queue/page.tsx", "r") as f:
-    lines = f.readlines()
-with open("app/queue/page.tsx", "w") as f:
-    f.writelines(lines[:722] + lines[977:])
